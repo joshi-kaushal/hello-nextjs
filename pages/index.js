bd4ec1994884e33c20b478ae0fb414b5d9c7c133
Kaushal Joshi <7joshikaushal@gmail.com>
2022-03-16 11:21:50 +0530

    ready for pre-rendering

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const router = useRouter();
@@ -12,39 +10,17 @@ export default function Home() {
   };
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Kaushal Joshi | Learning Next.js</title>
         <meta name="description" content="Learning Next.js" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Hello Next.js!</h1>
-
-        <section>
-          <Link href="/blog">
-            <h4>Blog</h4>
-          </Link>
-          <Link href="/product">
-            <h4>Products</h4>
-          </Link>
-          <button onClick={handleSubmit}>Go to Products</button>
-        </section>
+      <main>
+        <h1>Hello Next.js!</h1>
+        <p>I am being pre-rendered :)</p>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 }
